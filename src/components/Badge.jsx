ea7006472116ab835660ff8cb18bbb3538405e56
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-13 12:58:34 +0300

    initial commit

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,3 +1,14 @@
+import { motion } from "framer-motion";
+
 export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+  return (
+    <motion.span
+      initial={{}}
+      animate={{ scale: [1, 1.2, 1] }}
+      transition={{ duration: 0.3 }}
+      className="badge"
+    >
+      {caption}
+    </motion.span>
+  );
 }
